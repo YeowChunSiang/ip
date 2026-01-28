590ff64bfb9e4786d97e2139c7323672c344d149
YeowChunSiang <chunsiang27@gmail.com>
2026-01-28 20:50:46 +0800

    Add echo feature and bye command

--- src/main/java/Listo.java
+++ src/main/java/Listo.java
@@ -1,9 +1,22 @@
+import java.util.Scanner;
+
 public class Listo {
     public static void main(String[] args) {
-        String greeting = "Hello! I'm Listo\nWhat can I do for you?";
+        String greeting = "Hello! I'm Listo\nHow can I help you?";
         String goodbye = "Bye. Hope to see you again soon!";
 
         System.out.println(greeting);
+
+        // Echos user input, exits when user use the command "bye"
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            String input = scanner.nextLine();
+            if (input.equals("bye")) {
+                break;
+            }
+            System.out.println(input);
+        }
+
         System.out.println(goodbye);
     }
 }
