b2874cd542ab974c976d43776aa111f53cf61d2f
YeowChunSiang <chunsiang27@gmail.com>
2026-01-28 20:21:52 +0800

    Rename to Listo and add greet/exit

---
+++ src/main/java/Listo.java
@@ -0,0 +1,9 @@
+public class Listo {
+    public static void main(String[] args) {
+        String greeting = "Hello! I'm Listo\nWhat can I do for you?";
+        String goodbye = "Bye. Hope to see you again soon!";
+
+        System.out.println(greeting);
+        System.out.println(goodbye);
+    }
+}
