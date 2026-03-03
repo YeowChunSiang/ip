98fcf92e6134a0114a1a9cc4191a4b501613c99f
YeowChunSiang <chunsiang27@gmail.com>
2026-03-04 00:20:59 +0800

    Fix CheckStyle: Remove unused Region import

--- src/main/java/listo/Listo.java
+++ src/main/java/listo/Listo.java
@@ -16,7 +16,6 @@ import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.Region;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import javafx.application.Platform;
