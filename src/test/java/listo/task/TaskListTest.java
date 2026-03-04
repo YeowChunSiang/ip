3cef266a95bf6372c463ada9f77367d31eae0569
YeowChunSiang <chunsiang27@gmail.com>
2026-03-04 10:52:40 +0800

    A-MoreTesting: Add unit tests for duplicate detection and date logic

--- src/test/java/listo/task/TaskListTest.java
+++ src/test/java/listo/task/TaskListTest.java
@@ -3,6 +3,8 @@ package listo.task;
 import listo.exception.ListoException;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 public class TaskListTest {
     @Test
@@ -20,5 +22,25 @@ public class TaskListTest {
         tasks.addTask(new Todo("read"));
         tasks.deleteTask(0); // Delete the first task
         assertEquals(0, tasks.getSize());
+    }
+
+    @Test
+    public void containsDuplicate_duplicateTask_returnsTrue() {
+        TaskList taskList = new TaskList();
+        Task t1 = new Todo("read book");
+        taskList.addTask(t1);
+
+        Task t2 = new Todo("read book"); // Same description
+        assertTrue(taskList.containsDuplicate(t2)); // Should be true
+    }
+
+    @Test
+    public void containsDuplicate_differentTask_returnsFalse() {
+        TaskList taskList = new TaskList();
+        Task t1 = new Todo("read book");
+        taskList.addTask(t1);
+
+        Task t2 = new Todo("write code"); // Different description
+        assertFalse(taskList.containsDuplicate(t2)); // Should be false
     }
 }
